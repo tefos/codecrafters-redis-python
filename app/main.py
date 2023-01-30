97dde3bc223fdb6d57a3f0b55ed05a4991eef3c6
tefos <soydinigames@outlook.com>
2023-01-30 18:21:53 -0500

    Respond to PING

--- app/main.py
+++ app/main.py
@@ -9,7 +9,10 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    client_conn, _ = server_socket.accept() # wait for client
+
+    client_conn.recv(1024)
+    client_conn.send(b'+PONG\r\n')
 
 
 if __name__ == "__main__":
