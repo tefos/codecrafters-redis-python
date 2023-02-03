d17674f51c6504dd056078291b054d6c8de99be3
tefos <soydinigames@outlook.com>
2023-02-02 20:41:54 -0500

    Handle concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 # Uncomment this to pass the first stage
 import socket
+import threading
 
+def respond_to_ping(socket: socket.socket):
+    try:
+        while(socket.recv(1024)):
+            socket.send(b'+PONG\r\n')
+    except (ConnectionResetError, BrokenPipeError) as e:
+        pass
+    socket.close()
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,10 +17,10 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_conn, _ = server_socket.accept() # wait for client
-
-    while(client_conn.recv(1024)):
-        client_conn.send(b'+PONG\r\n')
+    while True:
+        client_socket, _ = server_socket.accept() # wait for client
+        client_thread = threading.Thread(target=respond_to_ping, args=(client_socket,), daemon=True)
+        client_thread.start()
 
 
 if __name__ == "__main__":
